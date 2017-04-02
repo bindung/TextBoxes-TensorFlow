cffcfa76a3781c146b8a21dc950b02a326a8ebf2
xiaodiu2010 <xiaodiu2010@gmail.com>
2017-04-02 01:05:50 -0400

    update

--- processing/txt_preprocessing.py
+++ processing/txt_preprocessing.py
@@ -121,6 +121,7 @@ def preprocess_for_train(image, labels, bboxes,
     
         # Distort image and bounding boxes.
         bboxes = tf.minimum(bboxes, 1.0)
+        bboxes = tf.maximum(bboxes, 0.0)
         dst_image, labels, bboxes, distort_bbox ,num= \
             distorted_bounding_box_crop(image, labels, bboxes,
                                         aspect_ratio_range=CROP_RATIO_RANGE)
