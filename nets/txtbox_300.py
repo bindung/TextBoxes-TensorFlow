6d354f4f5442fd1dd946c81e50c64a491777bc0f
xiaodiu2010 <xiaodiu2010@gmail.com>
2017-04-21 17:33:49 -0400

    update

--- nets/txtbox_300.py
+++ nets/txtbox_300.py
@@ -319,6 +319,7 @@ def text_losses(logits, localisations,
 		l_cross_pos = []
 		l_cross_neg = []
 		l_loc = []
+		n_poses = []
 		for i in range(len(logits)):
 			dtype = logits[i].dtype
 			with tf.name_scope('block_%i' % i):
