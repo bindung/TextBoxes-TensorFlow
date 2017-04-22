b731b940e053541cef912fec16f9801c5c7309a2
xiaodiu2010 <xiaodiu2010@gmail.com>
2017-04-22 00:17:55 -0400

    update

--- nets/txtbox_300.py
+++ nets/txtbox_300.py
@@ -283,7 +283,7 @@ def ssd_arg_scope(weight_decay=0.0005, data_format='NHWC'):
 	with slim.arg_scope([slim.conv2d, slim.fully_connected],
 						activation_fn=tf.nn.relu,
 						weights_regularizer=slim.l2_regularizer(weight_decay),
-						weights_initializer=tf.contrib.layers.xavier_initializer(),
+						weights_initializer=tf.contrib.layers.variance_scaling_initializer(factor=1.0,mode='FAN_AVG',uniform=True),
 						biases_initializer=tf.zeros_initializer()):
 		with slim.arg_scope([slim.conv2d, slim.max_pool2d],
 							padding='SAME',
@@ -301,7 +301,7 @@ def ssd_arg_scope(weight_decay=0.0005, data_format='NHWC'):
 # =========================================================================== #
 def text_losses(logits, localisations,
 			   glocalisations, gscores,
-			   match_threshold=TextboxNet.default_params.match_threshold,
+			   match_threshold,
 			   negative_ratio=3.,
 			   alpha=1.,
 			   label_smoothing=0.,
